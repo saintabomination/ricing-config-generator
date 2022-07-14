0a75ad2b2b958a9af84763ad870903e3ba09ec1c
saintabomination <saint.abomination@protonmail.com>
2022-07-14 16:55:32 +0200

    Improving Icon component's story

--- components/elements/Icon/Icon.component.tsx
+++ components/elements/Icon/Icon.component.tsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { x } from '@xstyled/styled-components';
 
 import type { IconName, IconPrefix } from '@fortawesome/fontawesome-common-types';
 
@@ -9,15 +10,18 @@ library.add(fas);
 library.add(fab);
 
 type Props = {
-  iconName: IconName;
-  iconType: IconPrefix;
+  name: IconName;
+  type: IconPrefix;
+  size?: number;
 };
 
-const Icon = ({ iconName, iconType }: Props): JSX.Element =>
+const Icon = ({ name, type, size = 1.0 }: Props): JSX.Element =>
   (
-    <div>
-      <FontAwesomeIcon icon={[iconType, iconName]} />
-    </div>
+    <x.div
+      fontSize={`${size}em`}
+    >
+      <FontAwesomeIcon icon={[type, name]} />
+    </x.div>
   );
 
 export default Icon;
